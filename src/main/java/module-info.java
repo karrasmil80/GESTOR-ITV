7c9d72728f1588d04d082d705c11163c2c3630a9
Pablo Zuil <zupablo3@gmail.com>
2025-06-13 15:45:11 +0200

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module dev.kkarrasmil80.gestoritv {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires kotlin.stdlib;
-
-
-    opens dev.kkarrasmil80.gestoritv to javafx.fxml;
-    exports dev.kkarrasmil80.gestoritv;
-}
