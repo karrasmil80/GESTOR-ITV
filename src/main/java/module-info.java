91a43a852d5ea73be7d708b0accb2da0ebcd094d
Pablo Zuil <zupablo3@gmail.com>
2025-06-16 14:42:15 +0200

    Añadidas las dependencias al modulo de java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,38 @@
 module dev.kkarrasmil80.gestoritv {
+
+    //Dependencias
+
+    //Java
     requires javafx.controls;
     requires javafx.fxml;
+
+    //Kotlin
+    requires kotlin.reflect;
     requires kotlin.stdlib;
 
+    //Logger
+    requires logging.jvm;
+    requires org.slf4j;
 
+    //Jdbi
+    requires org.jdbi.v3.core;
+    requires org.jdbi.v3.sqlobject;
+    requires org.jdbi.v3.kotlin;
+    requires org.jdbi.v3.sqlobject.kotlin;
+
+    //Koin
+    requires koin.core.jvm;
+
+    //Cache
+    requires com.github.benmanes.caffeine;
+
+
+    //App
     opens dev.kkarrasmil80.gestoritv to javafx.fxml;
     exports dev.kkarrasmil80.gestoritv;
+
+    //DAO
+    opens dev.kkarrasmil80.gestoritv.vehiculo.dao to org.jdbi.v3.core;
+    exports dev.kkarrasmil80.gestoritv.vehiculo.dao to kotlin.reflect;
+
 }
