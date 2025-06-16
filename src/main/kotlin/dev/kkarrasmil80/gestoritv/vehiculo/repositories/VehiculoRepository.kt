41d6ac69171252e9361bf3d34b9033e84589d7ca
Pablo Zuil <zupablo3@gmail.com>
2025-06-16 17:54:39 +0200

    Repositorio terminado

--- src/main/kotlin/dev/kkarrasmil80/gestoritv/vehiculo/repositories/VehiculoRepository.kt
+++ src/main/kotlin/dev/kkarrasmil80/gestoritv/vehiculo/repositories/VehiculoRepository.kt
@@ -1,5 +1,12 @@
 package dev.kkarrasmil80.gestoritv.vehiculo.repositories
 
-interface VehiculoRepository {
+import dev.kkarrasmil80.gestoritv.vehiculo.dao.VehiculoEntity
+import dev.kkarrasmil80.gestoritv.vehiculo.models.Vehiculo
 
+interface VehiculoRepository {
+    fun findAll(): List<Vehiculo>
+    fun findById(id: Int): Vehiculo?
+    fun save(vehiculo: Vehiculo): Int
+    fun deleteById(id: Int): Int
+    fun deleteAll(): Int
 }
--- src/main/kotlin/dev/kkarrasmil80/gestoritv/vehiculo/repositories/VehiculoRepositoryImpl.kt
+++ src/main/kotlin/dev/kkarrasmil80/gestoritv/vehiculo/repositories/VehiculoRepositoryImpl.kt
@@ -1,4 +1,41 @@
 package dev.kkarrasmil80.gestoritv.vehiculo.repositories
 
-class VehiculoRepositoryImpl {
+import dev.kkarrasmil80.gestoritv.vehiculo.dao.VehiculoDao
+import dev.kkarrasmil80.gestoritv.vehiculo.dao.VehiculoEntity
+import dev.kkarrasmil80.gestoritv.vehiculo.mapper.toEntity
+import dev.kkarrasmil80.gestoritv.vehiculo.mapper.toModel
+import dev.kkarrasmil80.gestoritv.vehiculo.models.Vehiculo
+import org.lighthousegames.logging.logging
+
+private val logger = logging()
+
+class VehiculoRepositoryImpl (
+    private val dao: VehiculoDao,
+) : VehiculoRepository {
+
+    override fun findAll(): List<Vehiculo> {
+        logger.debug { "Obteniendo todos los vehiculos" }
+        return dao.getAll().map { it.toModel() }
+    }
+
+    override fun findById(id: Int): Vehiculo? {
+        logger.debug { "Buscando vehiculo por id $id" }
+        return dao.getById(id)?.toModel()
+    }
+
+    override fun save(vehiculo: Vehiculo): Int {
+        logger.debug { "Guardando vehiculo ${vehiculo.id}" }
+        return dao.insert(vehiculo.toEntity())
+    }
+
+    override fun deleteById(id: Int): Int {
+        logger.debug { "Borrando vehiculo ${id}" }
+        return dao.deleteById(id)
+    }
+
+    override fun deleteAll(): Int {
+        logger.debug { "Borrando vehiculos" }
+        return dao.deleteAll()
+    }
+
 }
