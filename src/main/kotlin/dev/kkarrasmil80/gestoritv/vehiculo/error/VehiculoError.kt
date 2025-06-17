c04a9f07fd13297b94fb26a29be4db064ec43580
Pablo Zuil <zupablo3@gmail.com>
2025-06-17 16:31:01 +0200

    Ajustados los errores en VehiculoError

--- src/main/kotlin/dev/kkarrasmil80/gestoritv/vehiculo/error/VehiculoError.kt
+++ src/main/kotlin/dev/kkarrasmil80/gestoritv/vehiculo/error/VehiculoError.kt
@@ -1,8 +1,7 @@
 package dev.kkarrasmil80.gestoritv.vehiculo.error
 
 /**
- * Clase sellada que almacena todos los tipos de
- * errores que se podría dar en el programa.
+ *
  */
 
 sealed class VehiculoError(val message: String) {
